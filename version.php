be39b23607010dd60d6b4dd96aa8c7b9bca89b47
lushonline <56685070+lushonline@users.noreply.github.com>
2020-07-06 18:15:22 +0100

    Version bump to v2020070601

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2020061900; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020070601; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016052300; // Requires this Moodle version v3.1 see https://docs.moodle.org/dev/Releases.
 $plugin->component = 'tool_uploadexternalcontentresults'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = ['mod_externalcontent' => ANY_VERSION];
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0';
+$plugin->release = '1.1';
 
